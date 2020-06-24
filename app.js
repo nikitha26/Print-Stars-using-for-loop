d830785482e6b9eea69d5d006db0451dfacc987d
nikitha <mnikitha365@gmail.com>
2020-06-24 19:02:48 +0530

    Print stats using for loop

---
+++ app.js
@@ -0,0 +1,34 @@
+// let input = window.prompt('Enter any number')
+
+// for(i=1;i<=5;i++){
+//     var star = i;
+//     console.log('*' +star)
+//     document.write('* '+star)
+// }
+
+
+// one star log...
+// function fun1(i){
+//     for(i;i<=10;i++)
+//     console.log('*'+i)
+// }
+// fun1(1)
+
+
+
+// let name = 'johnroyjoewsdt'
+// let fullname = 'nikitha'
+// if(name>fullname){
+//     console.log(name)
+// }
+// else{
+//     console.log(fullname)
+// }
+
+
+var n = 3;
+
+for(i=0;i<=n;i++){
+        console.log(' * ')
+}
+
